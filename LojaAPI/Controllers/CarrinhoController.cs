cce6e0816fb7cdd712520d52401d5603be1722e5
caio.leite <caio.domingues@materamvar.com>
2018-01-22 16:05:09 -0200

    Primeiro Commit

---
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,43 @@
+﻿using LojaAPI.DAO;
+using LojaAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public string GetBuscaCarrinho(long id)
+        {
+            try
+            {
+                CarrinhoDAO dao = new CarrinhoDAO();
+                var carrinho = dao.Busca(id);
+                return carrinho.ToXml();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        //public string GetBuscaCarrinho(long id)
+        //{
+        //    try
+        //    {
+        //        CarrinhoDAO dao = new CarrinhoDAO();
+        //        var carrinho = dao.Busca(id);
+        //        return carrinho.ToXml();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+    }
+}
+
